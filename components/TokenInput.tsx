c7753f7a4f541d647f11e4f6dcb80e8cece86b22
MC <55539221+sarisssa@users.noreply.github.com>
2024-09-14 13:59:37 -0700

    fix: implement styling fix

--- components/TokenInput.tsx
+++ components/TokenInput.tsx
@@ -10,15 +10,15 @@ import { TokenInputData } from "./types";
 export const tokens: TokenInfo[] = [
   {
     symbol: "WETH",
-    icon: <Image src="/tokens/weth.png" width={24} height={24} />,
+    icon: <Image src="/tokens/weth.png" width={24} height={24} alt="WETH" />,
   },
   {
     symbol: "ETH",
-    icon: <Image src="/tokens/eth.png" width={24} height={24} />,
+    icon: <Image src="/tokens/eth.png" width={24} height={24} alt="ETH" />,
   },
   {
     symbol: "DAI",
-    icon: <Image src="/tokens/dai.png" width={24} height={24} />,
+    icon: <Image src="/tokens/dai.png" width={24} height={24} alt="DAI" />,
   },
 ];
 
@@ -52,25 +52,27 @@ export const TokenInput = forwardRef(
     useImperativeHandle(ref, () => ({ setSelectedToken }), [setSelectedToken]);
 
     return (
-      <div className="flex flex-nowrap p-4 my-0.5 h-24 bg-input-background dark:bg-dark-input-background rounded-xl border border-input-border border-opacity-0 focus-within:!border-opacity-[0.4] hover:border-opacity-[0.08] transition duration-[150ms]">
-        <input
-          className="focus:outline-0 bg-transparent h-fit overflow-auto placeholder-tigres-placeholder dark:placeholder-dark-tigres-placeholder text-4xl font-normal text-tigres-black dark:text-white"
-          type="number"
-          value={value}
-          placeholder="0"
-          onChange={(evt) => {
-            const value = evt.target.value.replace(/\D/g, "");
-            setValue(value);
-            onChangeData?.({
-              token: selectedToken,
-              value: +(value ?? 0),
-            });
-          }}
-        />
-        <div className="flex flex-col text-end flex-shrink-0">
+      <div className="flex items-center justify-between p-4 my-0.5 h-24 bg-input-background dark:bg-dark-input-background rounded-xl border border-input-border border-opacity-0 focus-within:!border-opacity-[0.4] hover:border-opacity-[0.08] transition duration-[150ms]">
+        <div className="flex-grow">
+          <input
+            className="w-full focus:outline-0 bg-transparent h-fit overflow-auto placeholder-tigres-placeholder dark:placeholder-dark-tigres-placeholder text-4xl font-normal text-tigres-black dark:text-white"
+            type="number"
+            value={value}
+            placeholder="0"
+            onChange={(evt) => {
+              const value = evt.target.value.replace(/\D/g, "");
+              setValue(value);
+              onChangeData?.({
+                token: selectedToken,
+                value: +(value ?? 0),
+              });
+            }}
+          />
+        </div>
+        <div className="flex flex-col items-end ml-2">
           <button
             onClick={() => setIsSelectingToken(true)}
-            className={`dark:text-white text-center h-fit py-1 pr-1 pl-2 ml-auto rounded-2xl font-semibold transition duration-[150ms] ${
+            className={`dark:text-white text-center h-fit py-1 pr-1 pl-2 rounded-2xl font-semibold transition duration-[150ms] ${
               selectedToken
                 ? "bg-tigres-row-button dark:bg-dark-tigres-row-button hover:bg-swap-row-hover hover:dark:bg-dark-swap-row-hover"
                 : "dark:bg-dark-button-active bg-button"
@@ -95,7 +97,7 @@ export const TokenInput = forwardRef(
           {selectedToken?.symbol === "ETH" &&
             walletBalance !== undefined &&
             isWalletConnected && (
-              <div className="flex flex-end pt-2 items-center">
+              <div className="flex items-center pt-2">
                 <span className="text-tigres-info text-sm font-normal">
                   Balance: {walletBalance.toFixed(3)}
                 </span>
